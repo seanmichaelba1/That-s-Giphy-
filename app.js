925e7c2a16bbd0152fe4f5b0c3aa9c15ad68c52e
Sean Bailey <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COMSeanmichaelba@gmail.com>
2020-09-25 18:12:46 -0500

    sean

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
 const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
-const API_KEY = 'S1sHTPi1WLsuzi3wmtKSLbU79vt6r2FY'
-
-
-const getGifs = (
-  url = `https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?api_key=S1sHTPi1WLsuzi3wmtKSLbU79vt6r2FY&q=phrase&limit=25&offset=0&rating=pg-13&lang=en`) => {
-  const getURL=`${corsAnywhere}${url}`
-  const headers = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      "Origin": "*"
+const API_KEY = 'a08MEcCriQFZgpyBORysdkIWKtQvGdwE'
+const getGifs = (searchTerm="string") => {
+const url=geturl(searchTerm) 
+  const results = document.querySelector('.results')
+  axios.get(url).then((res) => {
+    // console.log(res)
+    const giphsList = res.data.data;
+  
+    renderList(giphsList)
+    
+  
+    
+  });
+  function renderList(giphyData) {
+    const giphyImage = document.querySelector('.giphy-list')
+    giphyImage.innerHTML
+    for (let i = 0; i < giphyData.length; i++) {
+      let giphyItem = giphyData[i]
+      let giphyTag = document.createElement('img')
+      giphyTag.src = giphyItem.images.original.url;
+      giphyTag.class="gifImage"
+      giphyImage.appendChild(giphyTag)
+    }
   }
 }
-  const results = document.querySelector('.results')
-  axios.get(url, headers).then((res) => {
-    console.log(res)
-    const giphsList = res.data.data;
-    // console.log(response);
-    giphsList.forEach((gif) => {
-      let image = document.createElement('img')
-      image.crossOrigin = 'anonymous'
-      image.src = gif.url
-      results.appendChild(image)
-      console.log(gif.image)
-      console.log(gif.title);
-      const giphysDiv = document.querySelector(".giphys");
-      const gifsDiv = document.createElement("div");
+ 
 
-      const gifPic = document.createElement('p')
-      const button = document.querySelector('button')
-      button.addEventListener('click', async () => {
-        let input = input.value
-      })
-      
-    });
-    
+const form = document.querySelector('.spaces')
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      let input = e.target.searchbar.value
+      getGifs(input);
+    })
 
 
-  });
-};
-
-getGifs();
+function geturl(searchTerm) {
+  return `https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}=&offset=0&rating=&lang=en`
+    }
